518dca1f182574a71dc4a80672e5965f92fca6e4
kckerbypatel <kc.kerby-patel@umb.edu>
2017-05-13 19:09:08 -0400

    threshold is a ratio to volume in last non-thunder chunk

--- python/thunderDetector.py
+++ python/thunderDetector.py
@@ -7,21 +7,25 @@ class ThunderDetector:
         self.time_since_thunder=0
         self.minfbin=int(100/(44100/chunk))
         self.maxfbin = int(1000/(44100 / chunk))
+        self.prev_avg=0
 
 
     def detect(self, chunk, threshold):
         # print type(signal)
         # print type(self.delay_line)
         data = frombuffer(chunk, dtype='<i2')
-        #print(max(data))
-        fcontent = abs(fft.fft(data-mean(data)))
-        if mean(fcontent[self.minfbin:self.maxfbin])>threshold or (self.time_since_thunder==0 and mean(fcontent[self.minfbin:self.maxfbin])>threshold/10):
-            self.intensity= 255 * mean(fcontent[self.minfbin:self.maxfbin]) / max(abs(fcontent)) #normalize max brightness to 255
-            self.time_since_thunder=0
+        fcontent = abs(fft.fft(data - mean(data)))
+        if self.prev_avg==0:
+            self.prev_avg = mean(fcontent[self.minfbin:self.maxfbin])
         else:
-            self.intensity=0
-            self.time_since_thunder+1
-        return self.intensity
+            if mean(fcontent[self.minfbin:self.maxfbin])/self.prev_avg>threshold or (self.time_since_thunder==0 and mean(fcontent[self.minfbin:self.maxfbin])/self.prev_avg>threshold/10):
+                self.intensity= 255 * mean(fcontent[self.minfbin:self.maxfbin]) / max(abs(fcontent)) #normalize max brightness to 255
+                self.time_since_thunder=0
+            else:
+                self.intensity=0
+                self.prev_avg = mean(fcontent[self.minfbin:self.maxfbin])
+                self.time_since_thunder+1
+            return self.intensity
 
     def setindex(self):
         self.ledindex = random.uniform(0,256)

14eda5c513a865c84b1c3a8a8943ba4cebc997dd
kckerbypatel <kc.kerby-patel@umb.edu>
2017-05-13 19:13:31 -0400

    fancier intensity control

--- python/thunderDetector.py
+++ python/thunderDetector.py
@@ -8,6 +8,7 @@ class ThunderDetector:
         self.minfbin=int(100/(44100/chunk))
         self.maxfbin = int(1000/(44100 / chunk))
         self.prev_avg=0
+        self.prev_max=0
 
 
     def detect(self, chunk, threshold):
@@ -18,9 +19,13 @@ class ThunderDetector:
         if self.prev_avg==0:
             self.prev_avg = mean(fcontent[self.minfbin:self.maxfbin])
         else:
-            if mean(fcontent[self.minfbin:self.maxfbin])/self.prev_avg>threshold or (self.time_since_thunder==0 and mean(fcontent[self.minfbin:self.maxfbin])/self.prev_avg>threshold/10):
-                self.intensity= 255 * mean(fcontent[self.minfbin:self.maxfbin]) / max(abs(fcontent)) #normalize max brightness to 255
+            if mean(fcontent[self.minfbin:self.maxfbin])/self.prev_avg>threshold:
+                self.intensity= 255 #normalize max brightness to 255
+                self.prev_max=mean(fcontent[self.minfbin:self.maxfbin])
                 self.time_since_thunder=0
+            elif (self.time_since_thunder==0 and mean(fcontent[self.minfbin:self.maxfbin])/self.prev_avg>threshold/10):
+                self.intensity = 255 * mean(fcontent[self.minfbin:self.maxfbin]) / self.prev_max # normalize max brightness to 255
+                self.time_since_thunder = 0
             else:
                 self.intensity=0
                 self.prev_avg = mean(fcontent[self.minfbin:self.maxfbin])

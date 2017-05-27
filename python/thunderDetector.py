a3d7f8f48c313d646c90083c05bf49d57c24b18f
kckerbypatel <kc.kerby-patel@umb.edu>
2017-05-27 09:26:21 -0400

    frequency bin-wise thunder detection.

--- python/thunderDetector.py
+++ python/thunderDetector.py
@@ -5,30 +5,34 @@ class ThunderDetector:
         self.intensity=0
         self.ledindex=0
         self.time_since_thunder=0
-        self.minfbin=int(50/(44100/chunk))     #thunder is expected between these two frequencies
-        self.maxfbin = int(700/(44100 / chunk))
-        self.prev_avg=1
-        self.prev_max=1
+        self.minfbin=0    #thunder is expected between these two frequencies
+        self.maxfbin = int(2500/(44100 / chunk))
+        self.prev_avg=zeros(1, chunk)
+        self.prev_max=zeros(1,chunk)
+        self.initflag=1;
 
 
     def detect(self, chunk, onthreshold, offthreshold):
         data = frombuffer(chunk, dtype='<i2')
-        fcontent = abs(fft.fft(data - mean(data)))
-        level=mean(fcontent[self.minfbin:self.maxfbin])
-        if self.prev_avg==1:
-            self.prev_avg=level
+        fcontent = abs(fft.fft(data))
+        levels = fcontent[self.minfbin:self.maxfbin]
+        if self.initflag==1:
+            self.initflag=0
+            self.prev_avg = fcontent[self.minfbin:self.maxfbin]
         else:
-            if (self.time_since_thunder==0 and level/self.prev_avg>offthreshold and level<self.prev_max):
+            if (self.time_since_thunder==0 and any(levels >offthreshold*self.prev_avg)):
                 #continuing old thunder
-                self.intensity = 255 * level / self.prev_max # normalize max brightness to 255
+                self.intensity = min(255, 255 * max(levels) / max(self.prev_max)) # normalize max brightness to 255
+                if self.intensity==255:
+                    self.prev_max=levels
                 self.time_since_thunder = 0
-            elif level/self.prev_avg > onthreshold: #new thunder
+            elif any(levels > onthreshold*self.prev_avg): #new thunder
                 self.intensity= 255 #normalize max brightness to 255
-                self.prev_max=level
+                self.prev_max=levels
                 self.time_since_thunder=0
             else: #no thunder
                 self.intensity=0
-                self.prev_avg = mean(fcontent[self.minfbin:self.maxfbin])
+                self.prev_avg = (fcontent[self.minfbin:self.maxfbin]+self.prev_avg)/2
                 self.time_since_thunder=self.time_since_thunder+1
             return self.intensity
 

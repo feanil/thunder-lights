46981207d74a9db879ecfeef48cd71f918068431
Feanil Patel <feanil@gmail.com>
2017-05-27 08:01:21 -0400

    Handle multiple USB serial devices.
    It will pick some number of them at random to send data to. So
    sometimes lightning will be in multiple clouds and some times it
    won't be.

--- python/doThunder.py
+++ python/doThunder.py
@@ -1,19 +1,21 @@
 from pyaudio import PyAudio, paInt16
 from thunderDetector import ThunderDetector
 from serial import Serial
+from glob import glob
+import random
 
 pa = PyAudio()
 info = pa.get_host_api_info_by_index(0)
 
 numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
-        if (pa.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-            print "Input Device id ", i, " - ", pa.get_device_info_by_host_api_device_index(0, i).get('name')
+    if (pa.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print "Input Device id ", i, " - ", pa.get_device_info_by_host_api_device_index(0, i).get('name')
 
 chunk = 1024
 on_threshold = 4
 off_threshold = 2
-counter=0
+counter = 0
 
 detector = ThunderDetector(chunk)
 
@@ -26,23 +28,32 @@ stream = pa.open(format = paInt16,
 
 data=stream.read(chunk)
 
-ser = None
-#ser = Serial('/dev/ttyUSB2')
+num_devices_to_output_to = 0
+serial_device_names = glob("/dev/ttyUSB*")
+serial_devices = [ Serial(device_name) for device_name in serial_device_names ]
 
-while len(data)>0:
+while data:
     #check if there is thunder in the data
     detector.detect(data, on_threshold, off_threshold)
     #get new data
     data=stream.read(chunk)
     #change this print statement to output
     intensity = int(detector.intensity)
-    print intensity
-    if ser:
-        ser.write([intensity])
+
+    # Switch which devices to output to everytime it's quiet
+    if intensity > 0:
+        print intensity
+        if serial_devices:
+            random.shuffle(serial_devices)
+            num_devices_to_output_to = random.randint(0, len(serial_devices)-1)
+
+    # Write data to relevant serial devices.
+    for device in serial_devices[:num_devices_to_output_to]:
+        device.write([intensity])
 
 stream.stop_stream()
 stream.close()
-if ser:
-    ser.close()
+for device in serial_devices:
+    device.close()
 pa.terminate()
 

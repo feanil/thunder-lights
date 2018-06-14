2406c2fc6900feb89c865c4be75a07c4677bd50e
Feanil Patel <feanil@gmail.com>
2018-06-13 21:13:43 -0400

    Have a default random thunder if we're not sending data.

--- arduino/thunder/thunder.ino
+++ arduino/thunder/thunder.ino
@@ -5,8 +5,8 @@
 
 // Center Cloud
 #define PIXELS 100
-// Window Cloud
-//#define NUM_LEDS 60
+#define CHANCE_OF_MOVING_LOCATION 150
+#define CHANCE_OF_BEING_OFF 100
 
 
 #define PIN 15
@@ -20,7 +20,7 @@ void setup() {
     Serial.begin(115200);
     strip.begin();
     strip.show();
-    randomSeed(analogRead(0));
+    randomSeed(40);
 }
 
 void loop() {
@@ -28,12 +28,23 @@ void loop() {
       intensity = Serial.read();
       pattern_one(intensity);
 
+    } else {
+      if(random(CHANCE_OF_MOVING_LOCATION) == 1) {
+        pattern_one(0);
+      }
+      pattern_one(random(100)+100);
+      delay(random(50));
+    }
+
+    if(random(CHANCE_OF_BEING_OFF) == 1){
+      pattern_one(0);
+      delay(random(4000)+1000);
     }
 }
 
 void pattern_one(uint8_t intensity) {
     if(intensity == 0){
-      test_index = random(294)+3;
+      test_index = random(PIXELS-3)+3;
     }
     
     strip.setPixelColor(test_index-3, strip.Color(0,0,0));
@@ -41,9 +52,10 @@ void pattern_one(uint8_t intensity) {
     strip.setPixelColor(test_index-1, strip.Color(128,128,128));
     strip.setPixelColor(test_index, strip.Color(intensity,intensity,intensity));
     strip.setPixelColor(test_index+1, strip.Color(intensity,intensity,intensity));
-    strip.setPixelColor(test_index+2, strip.Color(intensity,intensity,intensity));
-    strip.setPixelColor(test_index+3, strip.Color(intensity,intensity,intensity));
+//    strip.setPixelColor(test_index+2, strip.Color(intensity,intensity,intensity));
+//    strip.setPixelColor(test_index+3, strip.Color(intensity,intensity,intensity));
     test_index += 1;
+    test_index = test_index % PIXELS;
     
     strip.setBrightness(intensity);
     strip.show();
